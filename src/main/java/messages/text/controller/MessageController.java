7a2b9a97ce9d3a2c80b8ce77f5bee012e3238fc0
Artem <artymk77@gmail.com>
2020-07-21 21:46:46 +0300

    add List of messages in controller

--- src/main/java/messages/text/controller/MessageController.java
+++ src/main/java/messages/text/controller/MessageController.java
@@ -4,12 +4,37 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @RestController
 @RequestMapping("text")
 public class MessageController {
+    public List<Map<String, String>> messages = new ArrayList<Map<String, String>>() {{
+        add(new HashMap<String, String>() {{
+            put("id", "1");
+            put("text", "Тихо струится река серебристая");
+        }});
+        add(new HashMap<String, String>() {{
+            put("id", "2");
+            put("text", "В царстве вечернем зеленой весны.");
+        }});
+        add(new HashMap<String, String>() {{
+            put("id", "3");
+            put("text", "Солнце садится за горы лесистые.");
+        }});
+        add(new HashMap<String, String>() {{
+            put("id", "4");
+            put("text", "Рог золотой выплывает луны.");
+        }});
+    }};
+
+
     @GetMapping
-    public String list(){
-        return "main";
+    public List<Map<String, String>> list() {
+        return messages;
     }
 
 }
