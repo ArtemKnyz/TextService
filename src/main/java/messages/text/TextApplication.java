e911a01644ba57fd72769dbc3c23a62e9f5dd8a3
Artem <artymk77@gmail.com>
2020-07-21 23:40:13 +0300

    added new branch and changed RestController

--- src/main/java/messages/text/TextApplication.java
+++ src/main/java/messages/text/TextApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TextApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TextApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TextApplication.class, args);
+    }
 
 }
--- src/main/java/messages/text/controller/MessageController.java
+++ src/main/java/messages/text/controller/MessageController.java
@@ -1,40 +1,39 @@
 package messages.text.controller;
 
+import messages.text.exceptions.NotFoundException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("text")
 public class MessageController {
-    public List<Map<String, String>> messages = new ArrayList<Map<String, String>>() {{
-        add(new HashMap<String, String>() {{
-            put("id", "1");
-            put("text", "Тихо струится река серебристая");
-        }});
-        add(new HashMap<String, String>() {{
-            put("id", "2");
-            put("text", "В царстве вечернем зеленой весны.");
-        }});
-        add(new HashMap<String, String>() {{
-            put("id", "3");
-            put("text", "Солнце садится за горы лесистые.");
-        }});
-        add(new HashMap<String, String>() {{
-            put("id", "4");
-            put("text", "Рог золотой выплывает луны.");
-        }});
+    public List<String> messages = new ArrayList<String>() {{
+        add("Тихо струится река серебристая");
+        add("В царстве вечернем зеленой весны.");
+        add("Солнце садится за горы лесистые.");
+        add("Рог золотой выплывает луны.");
     }};
 
 
     @GetMapping
-    public List<Map<String, String>> list() {
-        return messages;
+    public String list() {
+        list().length();
+        return messages.stream().sorted().findFirst().orElseThrow(NotFoundException::new);
     }
 
+    /*@GetMapping("{id}")
+    public List<String> getMessageById(@PathVariable String id) {
+        return getMessage(id);
+    }
+
+    private List<String> getMessage(@PathVariable String id) {
+        return messages.stream()
+                .filter(message -> message.get("id").equals(id))
+                .findFirst()
+                .orElseThrow(NotFoundException::new);
+    }*/
 }
+
